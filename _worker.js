625e41b412d085ef4b91fe16fb797cd9c3fb319a
bruceroe <61445155+bruceroe@users.noreply.github.com>
2024-08-29 08:01:23 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'f1b5454f-1d30-40dd-aa8b-4059556b6895';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
